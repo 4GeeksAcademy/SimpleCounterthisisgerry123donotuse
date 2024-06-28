ee00103045e7ca31c225fbc1438dcc41c77c74e6
Thisisgerry123 <167911356+Thisisgerry123@users.noreply.github.com>
2024-06-28 22:28:30 +0000

    Yeah

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,22 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+
+function SimpleCounter(props){
+    return (<div classNmae= "bigCounter">
+                <div className="seconds"></div>
+                <div className="calendar"><i class="far fa-clock"></i></div>
+                <div className="four"></div>
+                <div className="three"></div>
+                <div className="two"></div>
+                <div className="one"></div>
+    </div>);
+}
+
+
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+ReactDOM.render(<SimpleCounter />,
+    document.querySelector('#app')
+);
+    
 
